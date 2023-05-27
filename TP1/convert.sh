4c44f89f33060ec4388bc2aaad542513243fd749
Gustavo Maia <vm.gustavo31@gmail.com>
2023-05-27 11:34:53 -0300

    fix convert.sh

--- TP1/convert.sh
+++ TP1/convert.sh
@@ -1,3 +1,3 @@
 cp iterative.ipynb gustavo_maia__tp01.ipynb; \
-    poetry run jupyter nbconvert --to pdf gustavo_maia__tp01.ipynb \
+    poetry run jupyter nbconvert --to pdf gustavo_maia__tp01.ipynb; \
     rm gustavo_maia__tp01.ipynb
