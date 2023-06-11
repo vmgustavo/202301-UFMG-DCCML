66dcb633bd00e8e3a64836a1513c57df11fbab80
Gustavo Maia <vm.gustavo31@gmail.com>
2023-06-11 11:17:48 -0300

    create fit method

--- TP2/decision_stump.py
+++ TP2/decision_stump.py
@@ -1,86 +1,90 @@
+from typing import Any
+from operator import eq
+from collections import namedtuple
+
+import numpy as np
+from sklearn.metrics import accuracy_score
 from sklearn.utils.multiclass import unique_labels
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
 
 class DecisionStump(ClassifierMixin, BaseEstimator):
-    """ An example classifier which implements a 1-NN algorithm.
-
-    For more information regarding how to build your own classifier, read more
-    in the :ref:`User Guide <user_guide>`.
-
-    Parameters
-    ----------
-    demo_param : str, default='demo'
-        A parameter used for demonstation of how to pass and store paramters.
-
-    Attributes
-    ----------
-    X_ : ndarray, shape (n_samples, n_features)
-        The input passed during :meth:`fit`.
-    y_ : ndarray, shape (n_samples,)
-        The labels passed during :meth:`fit`.
-    classes_ : ndarray, shape (n_classes,)
-        The classes seen at :meth:`fit`.
-    """
+    X_: Any
+    y_: Any
+    classes_: Any
+    best_attr_test_: Any
 
     def __init__(self, demo_param='demo'):
         self.demo_param = demo_param
 
     def fit(self, X, y):
-        """A reference implementation of a fitting function for a classifier.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_features)
-            The training input samples.
-        y : array-like, shape (n_samples,)
-            The target values. An array of int.
-
-        Returns
-        -------
-        self : object
-            Returns self.
-        """
         # Check that X and y have correct shape
-        X, y = check_X_y(X, y)
+        X, y = check_X_y(X, y, dtype=None, y_numeric=True)
         # Store the classes seen during fit
         self.classes_ = unique_labels(y)
 
         self.X_ = X
         self.y_ = y
-        # Return the classifier
+
+        entropy_init = self._entropy_step(self.y_)
+
+        tests = [
+            lambda x: eq(x, 'x'),
+            lambda x: eq(x, 'o'),
+            lambda x: eq(x, 'b'),
+        ]
+
+        decisions = list()
+        decision = namedtuple('decision', ['attr', 'test', 'gain'])
+        for attr in range(self.X_.shape[1]):
+            for test in tests:
+                tested = test(self.X_[:, attr])
+                group0 = self.y_[np.where(tested)]
+                group1 = self.y_[np.where(~tested)]
+                entropy_new = (
+                    group0.shape[0] / self.y_.shape[0] * self._entropy_step(group0)
+                    + group1.shape[0] / self.y_.shape[0] * self._entropy_step(group1)
+                )
+
+                info_gain = entropy_init - entropy_new
+
+                decisions.append(decision(attr, test, info_gain))
+
+        self.best_attr_test_ = sorted(decisions, key=lambda x: x.gain, reverse=True)[0]
+
         return self
 
+    @staticmethod
+    def _entropy(a: int, b: int):
+        freq = a / (a + b)
+        return -1 * freq * np.log(freq)
+
+    def _entropy_step(self, arr: np.ndarray):
+        unique, counts = np.unique(arr, return_counts=True)
+        return (
+            self._entropy(counts[0], counts[1])
+            + self._entropy(counts[1], counts[0])
+        )
+
     def predict(self, X):
-        """ A reference implementation of a prediction for a classifier.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_features)
-            The input samples.
-
-        Returns
-        -------
-        y : ndarray, shape (n_samples,)
-            The label for each sample is the label of the closest sample
-            seen during fit.
-        """
         # Check is fit had been called
         check_is_fitted(self, ['X_', 'y_'])
 
         # Input validation
         X = check_array(X)
 
-        return self.y_[closest]
+        return
 
 
 if __name__ == '__main__':
-    from download_data import download_date
+    from download_data import download_data
 
-    data = download_date()
-    y = data.pop('')
+    data, target = download_data()
 
     model = DecisionStump()
-    model.fit()
-
+    model.fit(data, target)
+    print(accuracy_score(
+        y_pred=model.predict(data),
+        y_true=target,
+    ))
